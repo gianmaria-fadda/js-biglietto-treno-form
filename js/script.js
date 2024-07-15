3fa27c1264349084299f8c757c616ff46f3a9c54
Gianmaria Fadda <faddagianmaria10@gmail.com>
2024-07-15 10:40:26 +0200

    Completato esercizio

--- js/script.js
+++ js/script.js
@@ -1,37 +1,47 @@
-const km = prompt('Quanti chilometri vuoi percorrere?')
-console.log('km', km, typeof km)
+const calcForm = document.querySelector('form');
+calcForm.addEventListener(
+    'submit',
+    function (event) {
+        event.preventDefault();
+        console.log('event', event, typeof event);
 
-const age = prompt('Quanti anni hai?')
-console.log('age', age, typeof age)
-
-if (age < 18) {
-    console.log('Minorenne');
-    const price = km * 0.21
-    console.log('price', price, typeof price)
-
-    const discount = (price / 100) * 20
-    console.log('discount', discount, typeof discount)
-
-    const finalPrice = price - discount
-
-    alert('|PREZZO RIDOTTO| il prezzo del biglietto è €' + finalPrice.toFixed(2))
-}
-else if (age > 65) {
-    console.log('Over 65');
-    const price = km * 0.21
-    console.log('price', price, typeof price)
-
-    const discount = (price / 100) * 40
-    console.log('discount', discount, typeof discount)
-
-    const finalPrice = price - discount
-
-    alert('|PREZZO OVER 65| il prezzo del biglietto è €' + finalPrice.toFixed(2))
-}
-else {
-    console.log('Paga prezzo Pieno');
-    const price = km * 0.21
-    console.log('price', price, typeof price)
-
-    alert('il prezzo del biglietto è €' + price.toFixed(2))
-}   
+        const kmInput = document.getElementById('km');
+        const km = kmInput.value;
+        console.log('km', km, typeof km);
+        
+        const ageInput = document.getElementById('age');
+        const age = ageInput.value;
+        console.log('age', age, typeof age);
+        
+        if (age < 18) {
+            console.log('Minorenne');
+            const price = km * 0.21
+            console.log('price', price, typeof price)
+        
+            const discount = (price / 100) * 20
+            console.log('discount', discount, typeof discount)
+        
+            const finalPrice = price - discount
+        }
+        else if (age > 65) {
+            console.log('Over 65');
+            const price = km * 0.21
+            console.log('price', price, typeof price)
+        
+            const discount = (price / 100) * 40
+            console.log('discount', discount, typeof discount)
+        
+            const finalPrice = price - discount
+        }
+        else {
+            console.log('Paga prezzo Pieno');
+            const price = km * 0.21
+            console.log('price', price, typeof price)
+        
+            alert('il prezzo del biglietto è €' + price.toFixed(2))
+            const resultSpan = document.getElementById('result');
+            resultSpan.innerHTML = price.toFixed(2)
+        }   
+         
+    }
+)
